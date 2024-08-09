0cf72f775aef90c45adbb15061212bab380deaa3
Mukusha <sveta.gurinovitch@yandex.ru>
2024-08-09 15:46:44 +0300

    step_1

---
+++ src/main/java/com/nerzon/course/entity/Cat.java
@@ -0,0 +1,13 @@
+package com.nerzon.course.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cat {
+    private String name;
+    private int age;
+    private int weight;
+}
